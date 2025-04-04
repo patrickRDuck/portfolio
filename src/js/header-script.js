c43eb64f56ed181aba1ac1c6482892ea464ca2fe
Patrick Ribeiro Duarte <patrickrd@Laptop-de-Patrick.local>
2025-04-04 17:23:53 -0300

    added responsive to header

---
+++ src/js/header-script.js
@@ -0,0 +1,47 @@
+const header = document.querySelector('header')
+
+function responsiveHeader() {
+    if(window.innerWidth < 600) {
+        header.classList.add("responsive")
+
+        header.innerHTML = `
+            <strong>
+                PatrickRDuck &lt; / &gt;
+            </strong>
+            
+            <button>
+                <img src="/src/assets/svg/menu-icon.svg" alt="menu lateral">
+            </button>
+        `
+
+        const headerButton = document.querySelector("header.responsive button")
+
+        headerButton.addEventListener("click", () => {
+            console.log("ola mundo")
+        })
+    } else {
+        header.classList.remove("responsive")
+
+        header.innerHTML = `
+            <strong>
+                PatrickRDuck &lt; / &gt;
+            </strong>
+
+            <nav>
+                <a href="/index.html">Home</a>
+                <a href="/src/pages/about.html">About</a>
+                <a href="/src/pages/portfolio.html">Portfolio</a>
+            </nav>
+
+            <a href="https://github.com/patrickRDuck" target="_blank">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#FFF" class="bi bi-github" viewBox="0 0 16 16">
+                    <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
+                </svg>
+            </a>
+        `
+    }
+}
+
+responsiveHeader()
+
+window.addEventListener("resize", responsiveHeader)
